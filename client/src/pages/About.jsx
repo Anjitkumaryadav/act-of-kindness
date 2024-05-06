3499b442ac079fac294f9084b12fe7f213bcfc41
Anjitkumaryadav <anjitaniket@gmail.com>
2024-05-07 02:07:45 +0530

    final submit

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,27 @@
+export default function About() {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Act of Kindness
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              
+              Random acts of kindness have the power to brighten someone's day in unexpected ways. Whether it's holding the door open for a stranger, paying for someone's coffee, or simply offering a smile, these gestures can create a ripple effect of positivity. In a world often filled with stress and negativity, acts of kindness serve as reminders of the inherent goodness within people. They break down barriers and foster connections, reminding us of our shared humanity.
+            </p>
+
+            <p>
+            The beauty of random acts of kindness lies in their spontaneity and sincerity. They are not bound by any expectation of reciprocity or recognition; rather, they stem from a genuine desire to make someone else's day a little brighter. In a fast-paced society where people are often consumed by their own concerns, these small acts of compassion serve as gentle reminders to pause, look beyond ourselves, and extend a helping hand to those around us.
+            </p>
+
+            <p>
+            Moreover, the impact of a random act of kindness can extend far beyond the immediate moment. It has the potential to inspire others to pay it forward, creating a chain reaction of generosity and compassion. Whether big or small, each act contributes to creating a more empathetic and compassionate world. As the saying goes, "no act of kindness, no matter how small, is ever wasted.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
